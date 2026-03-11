81f0491af8ddce0e459d4dff5f4df7050a5404b6
jpelletier1 <44589723+jpelletier1@users.noreply.github.com>
2026-03-11 09:44:42 -0400

    Vite fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        wizard: resolve(__dirname, 'prototype-plg-automations/wizard.html')
+      }
+    }
   }
 })
